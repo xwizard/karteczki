bab09df79bf4bb3459dc9f80313ffbacf9b6cc19
xwizard <xwizard@server>
2015-10-04 22:37:05 +0200

    semantic works!

--- index.html.js
+++ index.html.js
@@ -1,6 +1,18 @@
 'use strict';
-var jQuery = require('jquery');
-require('semantic-ui/semantic.js');
-var $ = jQuery;
 
-$('.ui.dropdown').dropdown();
+var ko = require('knockout');
+
+function WebmailViewModel() {
+    // Data
+    var self = this;
+    self.folders = ['One', 'Two', 'Three', 'Four'];
+    self.chosenFolderId = ko.observable();
+
+    // Behaviours
+    self.goToFolder = function(folder) { self.chosenFolderId(folder); };
+    self.ok = function() {
+      console.log('OK!');
+    }
+};
+
+ko.applyBindings(new WebmailViewModel());
